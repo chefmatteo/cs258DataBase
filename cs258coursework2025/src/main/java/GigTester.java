897f11cf46905cb2baf02b949e90f25325eb6cb1
Chefmatteo <matthewngbusiness@gmail.com>
2025-12-11 17:56:20 +0000

    update test 4

--- cs258coursework2025/src/main/java/GigTester.java
+++ cs258coursework2025/src/main/java/GigTester.java
@@ -9,6 +9,8 @@ import java.util.Random;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 import java.time.LocalDateTime;
 public class GigTester {
